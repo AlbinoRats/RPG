0b74739f7249f9f56f70ac0e4a4926f1c23a357b
Qiyuan Chen <45timesover@gmail.com>
2014-06-13 02:14:06 -0400

    6/13/2014 prgress

--- RPG/Util/input.cpp
+++ RPG/Util/input.cpp
@@ -6,18 +6,22 @@ input::input()
 }
 bool input::poll()
 {
-	switch (event.type)
+	if(SDL_PollEvent(&event))
 	{
+		switch (event.type)
+		{
 		case SDL_QUIT:
 		{
-			return false;
+						 return false;
 		}
 		case SDL_KEYDOWN:
 		case SDL_KEYUP:
 		{
-				keyboard.setKeyState(event);
-				break;
+						  keyboard.setKeyState(event);
+						  break;
 		}
+		}
+		
 	}
 	return true;
 }
