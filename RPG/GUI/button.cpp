28c4274f9b5d2e86e6413e6be577de0f00171956
Qiyuan Chen <45timesover@gmail.com>
2014-06-14 02:39:33 -0400

    asd

--- RPG/GUI/button.cpp
+++ RPG/GUI/button.cpp
@@ -1,5 +1,17 @@
 #include "button.h"
-button::button(const char* path) :Entity(makeRect(0,0,0,0))
+button::button(const char* path) 
+:Entity(makeRect(0,0,0,0)),
+active(false),
+hover(false),
+buttonPic(NULL)
 {
 
+}
+bool button::isActive()
+{
+	return active;
+}
+bool button::isHover()
+{
+	return hover;
 }
--- RPG/GUI/button.h
+++ RPG/GUI/button.h
@@ -4,7 +4,10 @@
 class button:public Entity
 {
 private:
-
+	bool active, hover;
+	SDL_Surface *buttonPic;
 public:
 	button(const char*path);
+	bool isActive();
+	bool isHover();
 };
